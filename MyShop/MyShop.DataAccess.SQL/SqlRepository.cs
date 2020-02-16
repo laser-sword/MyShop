141e297697529348b695cb2a69d6bc665b0ccef6
Trace Raine <D17125503@mytudublin.ie>
2020-02-16 13:13:18 +0000

    EntityFramewrok again?

---
+++ MyShop/MyShop.DataAccess.SQL/SqlRepository.cs
@@ -0,0 +1,50 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SqlRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+
+
+
+        public IQueryable<T> Collection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
